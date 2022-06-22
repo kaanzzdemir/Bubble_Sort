5116bf8adb4242b6402f04a3c43a6842c0b3a7e7
Kaan ÖZDEMİR <172803062@ogr.cbu.edu.tr>
2022-06-22 15:40:24 +0300

    Add project files.

---
+++ Diziler/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] sayilar = new int[5] { 5, 32, 24, 11, 68 };
+
+foreach (var item in sayilar)
+{
+    Console.Write(item + " ");
+}
+
+int kucukDeger;
+
+for (int i = 0; i < sayilar.Length; i++)
+{
+    for(int j = 0; j < sayilar.Length; j++)
+    {
+        if (sayilar[i] < sayilar[j])
+        {
+            kucukDeger = sayilar[i];
+            sayilar[i] = sayilar[j];
+            sayilar[j] = kucukDeger;
+        }
+    }
+}
+Console.WriteLine("\n");
+foreach (var item in sayilar)
+{
+    Console.Write(item + " ");
+}
